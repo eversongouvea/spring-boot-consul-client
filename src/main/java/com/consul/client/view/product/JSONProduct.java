f4f339022143ede998b88c376850a8a37fe16237
Everson Gouvea <suporte.gouvea@gmail.com>
2019-10-31 09:24:16 -0300

    adicionado riboon

--- src/main/java/com/consul/client/view/product/JSONProduct.java
+++ src/main/java/com/consul/client/view/product/JSONProduct.java
@@ -1,16 +1,18 @@
 package com.consul.client.view.product;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.consul.client.properties.CloudProviderConfiguration;
 
-@RibbonClient(name= "product", configuration=CloudProviderConfiguration.class )
+@RibbonClient(name= "product",configuration = CloudProviderConfiguration.class)
+@FeignClient(name= "product",fallback = ProductCallback.class)
 public interface JSONProduct {
 
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/services")
+	@RequestMapping(method = RequestMethod.GET, value = "/product/desction")
     String getStores();
 	
 }
