a842ccb4d3155753cdee95002416bec41e3c901a
Everson Gouvea <suporte.gouvea@gmail.com>
2019-11-19 10:38:57 -0300

    DataSoucerConfig

--- src/main/java/com/consul/client/config/FF4j.java
+++ src/main/java/com/consul/client/config/FF4j.java
@@ -2,39 +2,32 @@ package com.consul.client.config;
 
 import org.ff4j.consul.ConsulConnection;
 import org.ff4j.consul.store.FeatureStoreConsul;
-import org.springframework.beans.factory.annotation.Value;
+import org.ff4j.consul.store.PropertyStoreConsul;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.google.common.net.HostAndPort;
 import com.orbitz.consul.Consul;
 
 @Configuration
 public class FF4j {
 
-	@Value("${spring.cloud.consul.host:localhost}")
-    private String consulHost;
-	
-    @Value("${spring.cloud.consul.port:8500}")
-    private int consulPort;
-	
+	@Autowired
+	Consul consul;
     
     @Bean
-    public org.ff4j.FF4j ff4j() {
+    public org.ff4j.FF4j getFF4j() {
     	
-        org.ff4j.FF4j ff4jObject = new org.ff4j.FF4j();
-        
-        Consul c = Consul.builder()
-        		         .withHostAndPort(HostAndPort.fromParts(consulHost, consulPort))
-        		         .build();
-        
-        ConsulConnection connection = new ConsulConnection(c);
+        ConsulConnection connection = new ConsulConnection(consul);
         FeatureStoreConsul featureStoreConsul = new FeatureStoreConsul(connection);
+        PropertyStoreConsul propertyStoreConsul = new org.ff4j.consul.store.PropertyStoreConsul(connection);
         
-        ff4jObject.setFeatureStore(featureStoreConsul);
-        ff4jObject.setPropertiesStore(new org.ff4j.consul.store.PropertyStoreConsul(connection));
+        org.ff4j.FF4j ff4j = new org.ff4j.FF4j();
+        ff4j.setFeatureStore(featureStoreConsul);
+        ff4j.setPropertiesStore(propertyStoreConsul);
+        ff4j.audit(true);
         
-        return ff4jObject;
+        return ff4j;
     }
     
 }
